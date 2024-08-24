b19eb8456e8558dd80104599504a53701131a006
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2024-08-24 21:01:19 +0200

    add persistant storage for ollama

--- hosts/common/containers/ollama.nix
+++ hosts/common/containers/ollama.nix
@@ -8,7 +8,7 @@
       image = "ollama/ollama";
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      # volumes = [ "${opts.paths.application_data}/Ollama:/root/.ollama" ]; # TODO: ENABLE THIS TO HAVE PERSISTENT STORAGE
+      volumes = [ "/mnt/data/appdata/ollama:/root/.ollama" ];
       ports = [ "11434:11434" ];
       environment = {
         TZ = opts.timeZone;
@@ -26,7 +26,7 @@
         "--no-healthcheck"
       ];
       volumes = [
-        # "${opts.paths.application_data}/OllamaWeb:/app/backend/data" # TODO: ENABLE THIS TO HAVE PERSISTENT STORAGE
+        "/mnt/data/appdata/ollama-web:/app/backend/data"
       ];
       ports = [ "8080:8080" ];
       environment = {
