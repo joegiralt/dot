0c7fd615f462f799d1a2336b56f1361ae1817eae
joegiralt <joe.m.giralt@gmail.com>
2024-08-24 17:42:36 +0200

    first pass

---
+++ users/common/base.nix
@@ -0,0 +1,92 @@
+{ pkgs, nixpkgs, username, opts, ... }: {
+
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    stateVersion = "24.11";
+  };
+
+  home.file = {
+
+    ".tool-versions" = {
+      enable = true;
+      recursive = true;
+      text =
+        let
+          versions = [
+            { tool = "bun"; version = "1.1.20"; }
+            { tool = "elixir"; version = "1.17.2-otp-27"; }
+            { tool = "erlang"; version = "27.0.1"; }
+            { tool = "golang"; version = "1.22.5"; }
+            { tool = "nodejs"; version = "22.5.1"; }
+            { tool = "ruby"; version = "3.2.1"; }
+            { tool = "zig"; version = "0.13.0"; }
+            { tool = "gleam"; version = "1.3.2"; }
+          ];
+        in
+        builtins.concatStringsSep "\n" (builtins.map (v: "${v.tool} ${v.version}") versions);
+    };
+
+    ".profile" = {
+      enable = true;
+      recursive = false;
+      target = ".profile";
+      executable = true;
+      text = ''
+        export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
+      '';
+    };
+
+    ".xsession" = {
+      enable = true;
+      executable = true;
+      recursive = true;
+      text = ''
+            export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
+      #     export GDK_SCALE=1.1
+      #     export GDK_DPI_SCALE=1.1
+      #     export QT_AUTO_SCREEN_SCALE_FACTOR=1.1
+      #     export QT_SCALE_FACTOR=1.1
+      #     export WINIT_X11_SCALE_FACTOR=1.1
+       '';
+    };
+
+  };
+
+  programs = {
+    home-manager.enable = true;
+    htop.enable = true;
+  };
+
+  systemd.user = {
+    enable = true;
+    startServices = "sd-switch";
+  };
+
+  news.display = "silent";
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  nix = {
+    package = pkgs.nix;
+    gc = {
+      automatic = true;
+      frequency = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    settings = {
+      trusted-users = [ "${username}" ];
+      allowed-users = [ "${username}" ];
+      trusted-substituters = [ "${username}" ];
+      show-trace = true;
+      auto-optimise-store = true;
+      fallback = true;
+      experimental-features = [ "nix-command" "flakes" "recursive-nix" ];
+    };
+  };
+}
---
+++ users/common/git.nix
@@ -0,0 +1,125 @@
+{ pkgs, nixpkgs-stable, opts, ... }:
+{
+
+  home.file = {
+    ".gitattributes" = {
+      enable = true;
+      target = ".gitattributes";
+      recursive = true;
+      text = ''
+        *.rb diff=ruby
+        *.rake diff=ruby
+        *.gemspec diff=ruby
+        * whitespace=!indent,trail,space
+        *.[ch] whitespace=indent,trail,space diff=cpp
+        *.sh whitespace=indent,trail,space eol=lf
+        *.perl eol=lf diff=perl
+        *.pl eof=lf diff=perl
+        *.pm eol=lf diff=perl
+        *.py eol=lf diff=python
+        *.bat eol=crlf
+        /Documentation/**/*.txt eol=lf
+        /command-list.txt eol=lf
+        /GIT-VERSION-GEN eol=lf
+        /mergetools/* eol=lf
+        /t/oid-info/* eol=lf
+        /Documentation/git-merge.txt conflict-marker-size=32
+        /Documentation/gitk.txt conflict-marker-size=32
+        /Documentation/user-manual.txt conflict-marker-size=32
+        /t/t????-*.sh conflict-marker-size=32
+      '';
+    };
+
+    ".gitignore" = {
+      enable = true;
+      target = ".gitignore";
+      recursive = true;
+      text = ''
+        .tmux/plugins/**
+        !.tmux/plugins/tpm
+        .customacs.d/elpa/**
+      '';
+    };
+
+    ".gitconfig" = {
+      enable = true;
+      target = ".gitconfig";
+      recursive = true;
+      text = ''
+        [user]
+          email = joe.m.giralt@gmail.com
+          name = joegiralt
+        [core]
+          attributesfile = ~/.gitattributes
+          editor = ${pkgs.neovim}/bin/nvim
+          pager = ${pkgs.delta}/bin/delta
+        [commit]
+          gpgsign = false
+        [init]
+          defaultBranch = main
+        [color]
+          branch = auto
+          diff = auto
+          interactive = auto
+          status = auto
+          ui = true
+        [alias]
+          a      = add
+          ap     = add --patch
+          br     = branch
+          ca     = commit --amend
+          cb     = checkout -b
+          cl     = clone --recursive -j8
+          cm     = commit -m
+          co     = checkout
+          cp     = cherry-pick
+          df     = diff
+          dfs    = diff --staged
+          fucked = reset --hard
+          l      = log
+          lg     = log --decorate --oneline --graph
+          s      = status
+          st     = status
+          wta    = worktree add
+          wtr    = worktree remove
+        [rerere]
+          enabled = false
+        [advice]
+          detachedHead = false
+        [filter "lfs"]
+          clean = git-lfs clean -- %f
+          smudge = git-lfs smudge -- %f
+          process = git-lfs filter-process
+          required = true
+        [interactive]
+          diffFilter = delta --color-only
+        [add.interactive]
+          useBuiltin = false # required for git 2.37.0
+        [delta]
+            navigate = true
+            light = false
+            features = side-by-side line-numbers decorations
+            syntax-theme = Dracula
+            plus-style = syntax "#003800"
+            minus-style = syntax "#3f0001"
+            zero-style = dim syntax
+        [delta "decorations"]
+            commit-decoration-style = bold yellow box ul
+            commit-style = raw
+            file-style = omit
+            file-decoration-style = none
+            hunk-header-decoration-style = blue box
+            hunk-header-file-style = red
+            hunk-header-line-number-style = "#067a00"
+            hunk-header-style = file line-number syntax
+        [merge]
+            conflictstyle = diff3
+        [diff]
+            colorMoved = default
+        [safe]
+        	directory = /etc/nixos
+      '';
+    };
+  };
+
+}
