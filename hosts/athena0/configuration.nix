2038a0af3bd86f8e7eacd1a451e8375c7d82970c
sreedevk <sreedev@icloud.com>
2024-08-24 22:52:27 +0000

    disable server GUI

--- hosts/athena0/configuration.nix
+++ hosts/athena0/configuration.nix
@@ -71,11 +71,11 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver.enable = false;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -84,7 +84,7 @@
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing.enable = false;
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
