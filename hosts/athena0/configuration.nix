833190766c374f0aa3b2fb8ac085e2091ed95a09
sreedevk <sreedev@icloud.com>
2024-08-25 14:15:27 +0000

    disabled password prompt for wheel group

--- hosts/athena0/configuration.nix
+++ hosts/athena0/configuration.nix
@@ -109,6 +109,7 @@
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  security.sudo.wheelNeedsPassword = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
