62d49d523863920d631eb8c12ce4ef16abd597cd
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2024-08-30 16:42:03 +0200

    sets networking.firewall to checkReversePath = "loose"

--- hosts/common/services/tailscale.nix
+++ hosts/common/services/tailscale.nix
@@ -2,10 +2,12 @@
 {
   networking.firewall =
     {
+      checkReversePath = "loose";
       allowedTCPPorts = [ 41641 ];
       allowedUDPPorts = [ 41641 ];
     };
   services.tailscale = {
+    useRoutingFeatures = "both"; # (or "server"/client", if you want to use the machine itself as an exit node)
     enable = true;
     port = 41641;
     openFirewall = true;
