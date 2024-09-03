533ff78c10ac1f7417dea670d63596843abf1d03
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2024-09-04 00:14:19 +0200

    add volumes to plex container

--- hosts/common/containers/plex.nix
+++ hosts/common/containers/plex.nix
@@ -5,6 +5,8 @@
     "d /mnt/data/appdata/plex/database 0755 ${opts.adminUID} ${opts.adminGID} -"
     "d /mnt/data/appdata/plex/transcode 0755 ${opts.adminUID} ${opts.adminGID} -"
     "d /mnt/data/media/music 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d /mnt/data/media/film 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d /mnt/data/media/tv 0755 ${opts.adminUID} ${opts.adminGID} -"
   ];
 
   virtualisation.oci-containers.containers = {
@@ -16,6 +18,8 @@
         "/mnt/data/appdata/plex/database/:/config"
         "/mnt/data/appdata/plex/transcode/:/transcode"
         "/mnt/data/media/music:/music"
+        "/mnt/data/media/film:/movies"
+        "/mnt/data/media/tv:/tv"
       ];
       ports = [ "9009:32400" ];
       environmentFiles = [ config.age.secrets.plex-env.path ];
