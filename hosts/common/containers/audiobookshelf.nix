8a0b463530fa33a84d31aa3712891162f48435d9
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2024-09-07 01:39:43 +0200

    fixes typo

--- hosts/common/containers/audiobookshelf.nix
+++ hosts/common/containers/audiobookshelf.nix
@@ -1,54 +1,37 @@
 { config, lib, pkgs, opts, ... }: {
-  networking.firewall.allowedTCPPorts =
-    builtins.map pkgs.lib.strings.toInt (with opts.ports; [
-      audiobookshelf
-    ]);
+  networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ audiobookshelf ]);
 
-  systemd.tmpfiles.rules =
-    let
-      paths = opts.paths;
-      admin = opts;
-    in
-    [
-      "d ${paths.audiobooks} 0755 ${admin.adminUID} ${admin.adminGID} -"
-      "d ${paths.podcasts} 0755 ${admin.adminUID} ${admin.adminGID} -"
-      "d ${paths.app-data}/audiobookshelf/metadata 0755 ${admin.adminUID} ${admin.adminGID} -"
-      "d ${paths.app-data}/audiobookshelf/config 0755 ${admin.adminUID} ${admin.adminGID} -"
-    ];
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.audiobooks} 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d ${opts.paths.podcasts} 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d ${opts.paths.app-data}/audiobookshelf/metadata 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d ${opts.paths.app-data}/audiobookshelf/config 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
 
   virtualisation.oci-containers.containers = {
-    audiobookshelf =
-      let
-        admin = opts;
-        paths = opts.paths;
-        ports = opts.ports;
-        lanAddress = opts.lanAddress;
-        timeZone = opts.timeZone;
-        adminUID = opts.adminUID;
-        adminGID = opts.adminGID;
-      in
-      {
-        autoStart = true;
-        image = "ghcr.io/advplyr/audiobookshelf:latest";
-        extraOptions = [
-          "--no-healthcheck"
-        ];
-        volumes = [
-          "${paths.audiobooks}:/audiobooks"
-          "${paths.podcasts}:/podcasts"
-          "${paths.app-data}/audiobookshelf/metadata:/metadata"
-          "${paths.app-data}/audiobookshelf/config:/config"
-        ];
-        ports = [ "${ports.audiobookshelf}:80" ];
-        labels = {
-          "kuma.audiobookshelf.http.name" = "Audiobookshelf";
-          "kuma.audiobookshelf.http.url" = "http://${lanAddress}:${ports.audiobookshelf}/healthcheck";
-        };
-        environment = {
-          TZ = timeZone;
-          PUID = adminUID;
-          PGID = adminGID;
-        };
+    "audiobookshelf" = {
+      autoStart = true;
+      image = "ghcr.io/advplyr/audiobookshelf:latest";
+      extraOptions = [
+        "--no-healthcheck"
+        # "--network=host"
+      ];
+      volumes = [
+        "${opts.paths.audiobooks}:/audiobooks"
+        "${opts.paths.podcasts}:/podcasts"
+        "${opts.paths.app-data}/audiobookshelf/metadata:/metadata"
+        "${opts.paths.app-data}/audiobookshelf/config:/config"
+      ];
+      ports = [ "13378:80" ];
+      labels = {
+        "kuma.audiobookshelf.http.name" = "Audiobookshelf";
+        "kuma.audiobookshelf.http.url" = "http://${opts.lanAddress}:${opts.ports.audiobookshelf}/healthcheck";
       };
+      environment = {
+        TZ = opts.timeZone;
+        PUID = opts.adminUID;
+        PGID = opts.adminGID;
+      };
+    };
   };
 }
