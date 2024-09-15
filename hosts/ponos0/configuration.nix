fc342b2837378826179a0921da7dae0701df9f51
Joe Giralt <joe.m.giralt@gmail.com>
2024-09-15 13:06:45 +0200

    adds tryusted users

--- hosts/ponos0/configuration.nix
+++ hosts/ponos0/configuration.nix
@@ -288,6 +288,7 @@
     };
     package = pkgs.nixFlakes;
     extraOptions = ''
+        trusted-users = root admin
         extra-substituters = https://devenv.cachix.org;
         extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=;
     '';
