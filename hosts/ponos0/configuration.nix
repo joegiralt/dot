42da61a3079cf7d71cee16464bca798d89811b08
Joe Giralt <joe.m.giralt@gmail.com>
2024-09-15 12:52:20 +0200

    adds extra optons

--- hosts/ponos0/configuration.nix
+++ hosts/ponos0/configuration.nix
@@ -287,6 +287,10 @@
       experimental-features = [ "nix-command" "flakes" ];
     };
     package = pkgs.nixFlakes;
+    extraOptions = ''
+        extra-substituters = https://devenv.cachix.org;
+        extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=;
+    '';
   };
 
   system.switch = {
