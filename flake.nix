b0dbdaf13eac88f2b7b9c0c34d16a1e59aa6f6f8
Joe Giralt <joe.m.giralt@gmail.com>
2025-08-22 21:57:49 +0200

    update nixOS from 24 -> 25

--- flake.nix
+++ flake.nix
@@ -1,16 +1,24 @@
 {
-  description = "A very basic flake";
+  description = "A very basic flake (NixOS 25.05)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable&shallow=1";
+    # Track the 25.05 release branch (stable)
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05?shallow=1";
+
+    # Keep Home Manager in lockstep with the OS release
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # If you want select bleeding-edge packages while staying on stable OS,
+    # uncomment this and import from `unstable` where needed.
+    # unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable?shallow=1";
   };
 
   outputs = {
@@ -18,8 +26,10 @@
     nixpkgs,
     home-manager,
     agenix,
+    # unstable,  # uncomment if using the optional input above
     ...
-  } @ inputs: let
+  } @ inputs:
+  let
     opts = import ./opts.nix;
 
     systems = {
@@ -29,9 +39,9 @@
 
     mkFormatters = systemsl:
       builtins.foldl'
-      (output: sys: output // {${sys} = nixpkgs.legacyPackages."${sys}".nixpkgs-fmt;})
-      {}
-      (nixpkgs.lib.attrValues systemsl);
+        (output: sys: output // { ${sys} = nixpkgs.legacyPackages."${sys}".nixpkgs-fmt; })
+        {}
+        (nixpkgs.lib.attrValues systemsl);
 
     mkSystem = pkgs: system: hostname:
       pkgs.lib.nixosSystem {
@@ -39,10 +49,13 @@
         modules = [
           (import ./hosts/${hostname}/configuration.nix)
           agenix.nixosModules.default
+          # If mixing stable base with select unstable pkgs, you can pass them via specialArgs:
+          # { _module.args.unstable = import inputs.unstable { inherit system; }; }
         ];
         specialArgs = {
           inherit system hostname inputs;
           opts = opts // (import ./hosts/${hostname}/opts.nix);
+          # unstable = import inputs.unstable { inherit system; }; # if using unstable
         };
       };
 
@@ -55,10 +68,15 @@
         ];
         extraSpecialArgs = {
           inherit system username host;
-          opts = opts // (import ./hosts/${host}/opts.nix) // (import ./users/${username}/opts.nix);
+          opts =
+            opts
+            // (import ./hosts/${host}/opts.nix)
+            // (import ./users/${username}/opts.nix);
+          # unstable = import inputs.unstable { inherit system; }; # if using unstable
         };
       };
-  in {
+  in
+  {
     # Formatters for all systems
     formatter = mkFormatters systems;
 
