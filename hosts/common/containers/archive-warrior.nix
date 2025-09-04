4ceac1172e38b192828488249360e4fb582ab2e4
Joe Giralt <joe.m.giralt@gmail.com>
2025-09-04 17:53:24 +0200

    fixes wget issue in container

--- hosts/common/containers/archive-warrior.nix
+++ hosts/common/containers/archive-warrior.nix
@@ -24,6 +24,7 @@
 
     volumes = [
       "${opts.paths.app-data}/archiveteam-warrior:/data"
+      "${opts.paths.app-data}/archiveteam-warrior/wget-at:/usr/local/bin/wget-at:ro"
     ];
 
     # only used if you didn't switch to --network=host
@@ -31,7 +32,7 @@
 
     labels = {
       "kuma.archiveteam-warrior.http.name" = "ArchiveTeam Warrior";
-      "kuma.archiveteam-warrior.http.url"  = "http://${opts.lanAddress}:${toString opts.ports.warrior}";
+      "kuma.archiveteam-warrior.http.url" = "http://${opts.lanAddress}:${toString opts.ports.warrior}";
     };
 
     environment = {
@@ -39,19 +40,19 @@
       # these are mostly no-ops for this image, harmless to leave:
       PUID = toString opts.adminUID;
       PGID = toString opts.adminGID;
-
-      DOWNLOADER        = opts.warriorDownloader or "joegiralt";
-      SELECTED_PROJECT  = opts.warriorProject   or "auto";
-      CONCURRENT_ITEMS  = toString (opts.warriorConcurrent or 4);
+      
+      DOWNLOADER = opts.warriorDownloader or "joegiralt";
+      SELECTED_PROJECT = opts.warriorProject   or "auto";
+      CONCURRENT_ITEMS = toString (opts.warriorConcurrent or 4);
 
       WGET_AT = "/usr/local/bin/wget-lua";
     };
   };
 
   systemd.services."podman-archiveteam-warrior" = {
-    after  = [ "network-online.target" ];
-    wants  = [ "network-online.target" ];
-    
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
     unitConfig.RequiresMountsFor = [ "${opts.paths.app-data}" ];
 
     serviceConfig = {
