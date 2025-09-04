7ac53f150f728b1c251ae7cfbc9a2c429ed0639d
Joe Giralt <joe.m.giralt@gmail.com>
2025-09-04 18:29:54 +0200

    updates archive-warrior container to get wget-liua to work

--- hosts/common/containers/archive-warrior.nix
+++ hosts/common/containers/archive-warrior.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, opts, ... }:
 let
-  # Shim must use a container-available interpreter, not a Nix-store bash.
   wgetAtShim = pkgs.writeTextFile {
     name = "warrior-wget-at";
     executable = true;
@@ -33,26 +32,29 @@ in
 
     volumes = [
       "${opts.paths.app-data}/archiveteam-warrior:/data"
-      # Mount our /bin/sh-based shim where Seesaw looks for it:
       "${wgetAtShim}:/usr/local/bin/wget-at:ro"
       "${wgetAtShim}:/usr/bin/wget-at:ro"
+      "${wgetAtShim}:/usr/bin/wget-lua:ro"
     ];
 
     ports = [ "${toString opts.ports.warrior}:8001" ];
 
     labels = {
       "kuma.archiveteam-warrior.http.name" = "ArchiveTeam Warrior";
-      "kuma.archiveteam-warrior.http.url" = "http://${opts.lanAddress}:${toString opts.ports.warrior}";
+      "kuma.archiveteam-warrior.http.url"  = "http://${opts.lanAddress}:${toString opts.ports.warrior}";
     };
 
     environment = {
-      TZ = opts.timeZone;
+      TZ   = opts.timeZone;
       PUID = toString opts.adminUID;
       PGID = toString opts.adminGID;
 
-      DOWNLOADER = opts.warriorDownloader or "joegiralt";
+      DOWNLOADER       = opts.warriorDownloader or "joegiralt";
       SELECTED_PROJECT = opts.warriorProject    or "auto";
       CONCURRENT_ITEMS = toString (opts.warriorConcurrent or 4);
+
+      WGET_AT  = "/usr/local/bin/wget-at";
+      WGET_LUA = "/usr/local/bin/wget-lua";
     };
   };
 }
