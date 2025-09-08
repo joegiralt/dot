080832a39d83ca8044502e0feb439a71530c3803
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2025-09-09 00:12:47 +0200

    cuda btop

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Keep Home Manager in lockstep with the OS release
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
