e4413d678a6a613168bb1db1fbe6e8ffd9c4739d
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2025-09-09 10:04:02 +0200

    fixes issue with VScode

--- users/common/vscode.nix
+++ users/common/vscode.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = with pkgs.vscode-extensions; [
+  programs.vscode = {
+    enable = true;
+    extensions =
+      (with pkgs.vscode-extensions; [
         bbenoist.nix
         ms-python.python
         ms-azuretools.vscode-docker
         ms-vscode-remote.remote-ssh
-        (pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "remote-ssh-edit";
-            publisher = "ms-vscode-remote";
-            version = "0.47.2";
-            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-          }
-        ])
+      ])
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
       ];
-    })
-  ];
+  };
 }
