14452fa14c1e04c4a7885cf778c5f30d3f1932fb
Joe Giralt <joseph.giralt@tunecore.com>
2025-09-11 19:28:59 +0200

    adds carcosa to secrets

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,12 @@
 let
   admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKb/dFPcj5XiylyXyPQH+SAZP6ce3PkdgVaLIPvnaL4g";
   josephgiralt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNLo4EXLOhYMQwi1cozZnSRbG7WnMyULHWzoag3wYff";
+  carcosa = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGf5kqPOJkt4VWH7yUhTxX3OhoL7a4g8wsMgIbBWuj7";
 
   users = [
     admin
     josephgiralt
+    carcosa
   ];
 
   athena0 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLasQWV2PpautvpHtLHGdEArpJdmBAFaBxQ+zbonBF5";
