cde968449d4540b01aa9bd6b328ea11e9e8422ac
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2025-09-11 18:40:41 +0200

    fix homemanager issue

--- users/carcosa/default.nix
+++ users/carcosa/default.nix
@@ -1,53 +1,64 @@
-{ config, pkgs, inputs, ... }:
-
 {
-  # Make sure the nixGL HM module is imported so `config.lib.nixGL.wrap` exists.
-  # If your nixgl input exposes a different name, try `.default` instead of `.nixGL`.
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  onlyIf = cond: xs: if cond then xs else [];
+  gl     = pkg: config.lib.nixGL.wrap pkg;
+  gloff  = pkg: config.lib.nixGL.wrapOffload pkg;
+in
+{
   imports = [
-    inputs.nixgl.homeManagerModules.nixGL
     ../common/base.nix
-    # ../common/atuin.nix
-    # ../common/beets.nix
-    # ../common/btop.nix
-    # ../common/core-max.nix
-    # ../common/git.nix
-    # ../common/github.nix
-    # ../common/vscode.nix
-    # ../common/zsh.nix
   ];
 
-  # nixGL configuration
   nixGL = {
-    inherit (inputs.nixgl) packages;   # expects nixgl to be a flake input
-    defaultWrapper = "mesa";           # AMD/Intel iGPU path
-    offloadWrapper  = "nvidia";        # NVIDIA offload wrapper if needed
-    vulkan.enable   = true;
-    installScripts  = [ "mesa" "nvidia" ];
+    inherit (inputs.nixgl) packages;
+    defaultWrapper = "mesa";
+    offloadWrapper = "nvidia";
+    vulkan.enable = true;
+    installScripts = [ "mesa" "nvidia" ];
   };
-
-  # Convenience alias
-  # (only valid because the nixGL HM module is imported above)
-  _module.args.wrapGL = config.lib.nixGL.wrap;
 
   home.packages =
     let
-      # Helper: conditionally include packages
-      onlyIf = cond: pkgsList: if cond then pkgsList else [];
-
-      # Helper: wrap with nixGL on Linux GUI apps
-      gl = pkg: config.lib.nixGL.wrap pkg;
-
-      gui-packages =
-        builtins.concatLists [
-          # Slack is x86_64-linux only (Electron binary). Gate it.
-          (onlyIf (pkgs.system == "x86_64-linux") [
-            (gl pkgs.slack)
-          ])
-        ];
+      gui-packages = builtins.concatLists [
+        # Slack is x86_64-linux only; wrap with nixGL
+        (onlyIf (pkgs.system == "x86_64-linux") [
+          (gl pkgs.slack)
+        ])
+        
+      ];
 
       cli-packages = with pkgs; [
         agenix
       ];
     in
-      gui-packages ++ cli-packages;
+    builtins.concatLists [
+      gui-packages
+      cli-packages
+    ];
+
+  services.ssh-agent.enable = true;
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    extraOptionOverrides = {
+      StrictHostKeyChecking = "no";
+      LogLevel = "ERROR";
+    };
+    matchBlocks = {
+      "*" = { userKnownHostsFile = "/dev/null"; };
+      # "github.com" = {
+      #   hostname = "github.com";
+      #   user = "git";
+      #   identitiesOnly = true;
+      #   identityFile = "~/.ssh/id_ed25519";
+      # };
+    };
+  };
 }
