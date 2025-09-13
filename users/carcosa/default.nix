17bdaaf0c16798c3d96392070f9d3a6cfe6e75e1
sreedevk <sreedev@icloud.com>
2025-09-13 22:58:24 +0000

    added stylix

---
+++ users/carcosa/default.nix
@@ -0,0 +1,75 @@
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  onlyIf = cond: xs: if cond then xs else [ ];
+  gl = pkg: config.lib.nixGL.wrap pkg;
+  glOff = pkg: config.lib.nixGL.wrapOffload pkg;
+in
+{
+  imports = [
+    ../common/base.nix
+    ../common/mise.nix
+    ../common/wezterm.nix
+    ../common/stylix.nix
+  ];
+
+  nixGL = {
+    inherit (inputs.nixgl) packages;
+    defaultWrapper = "mesa";
+    offloadWrapper = "nvidia";
+    vulkan.enable = true;
+    installScripts = [
+      "mesa"
+      "nvidia"
+    ];
+  };
+
+  home.packages =
+    let
+      gui-packages = builtins.concatLists [
+        # Slack is x86_64-linux only; wrap with nixGL
+        (onlyIf (pkgs.system == "x86_64-linux") [
+          (gl pkgs.slack)
+        ])
+        (onlyIf (pkgs.system == "x86_64-linux") [
+          (pkgs.zed-editor)
+        ])
+      ];
+
+      cli-packages = with pkgs; [
+        tokei
+        agenix
+      ];
+    in
+    builtins.concatLists [
+      gui-packages
+      cli-packages
+    ];
+
+  services.ssh-agent.enable = true;
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    extraOptionOverrides = {
+      StrictHostKeyChecking = "no";
+      LogLevel = "ERROR";
+    };
+    matchBlocks = {
+      "*" = {
+        userKnownHostsFile = "/dev/null";
+      };
+      # "github.com" = {
+      #   hostname = "github.com";
+      #   user = "git";
+      #   identitiesOnly = true;
+      #   identityFile = "~/.ssh/id_ed25519";
+      # };
+    };
+  };
+}
