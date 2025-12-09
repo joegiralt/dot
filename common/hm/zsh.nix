e47211105bad3d8bd94f01cbc7d44a60dc617022
Joe Giralt <joseph.giralt@tunecore.com>
2025-12-09 18:52:17 +0100

    add Gemfile to project

--- common/hm/zsh.nix
+++ common/hm/zsh.nix
@@ -41,7 +41,7 @@
         fi
         podman ps -q --filter name="$container_name" | xargs -I {} podman logs {}
       }
-      
+
       rake() {
         GEM_HOME=/dev/null \
         GEM_PATH=/dev/null \
--- common/os/services/tailscale-auto-health.nix
+++ common/os/services/tailscale-auto-health.nix
@@ -1,10 +1,15 @@
-{ config, pkgs, opts, ... }:
+{
+  config,
+  pkgs,
+  opts,
+  ...
+}:
 
 {
   systemd.tmpfiles.rules = [
     "d /var/lib/tailscale-exit-health 0755 ${opts.adminUID} ${opts.adminGID} -"
   ];
-  
+
   systemd.services."tailscale-auto-health" = {
     description = "Tailscale Exit Node Health Checker (auto cycle on failure)";
     after = [ "network-online.target" ];
@@ -26,7 +31,7 @@
     wantedBy = [ "timers.target" ];
     timerConfig = {
       OnBootSec = "5min";
-      OnUnitActiveSec = "10min";  # run every 10 minutes
+      OnUnitActiveSec = "10min"; # run every 10 minutes
       Unit = "tailscale-auto-health.service";
     };
   };
